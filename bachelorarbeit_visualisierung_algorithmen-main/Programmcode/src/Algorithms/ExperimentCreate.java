69516ca1857ad6df883463a292b9ec71654a9dc3
Cedesc <christiandohmen1910@gmail.com>
2022-09-21 14:24:47 +0200

    added comments

--- bachelorarbeit_visualisierung_algorithmen-main/Programmcode/src/Algorithms/ExperimentCreate.java
+++ bachelorarbeit_visualisierung_algorithmen-main/Programmcode/src/Algorithms/ExperimentCreate.java
@@ -4,6 +4,9 @@ import abstractAlgorithmus.AbstractAlgorithm;
 import supportClasses.types;
 
 // OWN TEST STUFF
+/**
+ * Just creates multiple "Experiment"-Arrays for testing the zooming, scrolling and resizing of the window
+ */
 public class ExperimentCreate extends AbstractAlgorithm {
 
     @Override
@@ -29,6 +32,16 @@ public class ExperimentCreate extends AbstractAlgorithm {
         create_Experiment(types.NUMBER, 18);
         create_Experiment(types.NUMBER, 19);
         create_Experiment(types.NUMBER, 20);
+        create_Experiment(types.NUMBER, 31);
+        create_Experiment(types.NUMBER, 32);
+        create_Experiment(types.NUMBER, 33);
+        create_Experiment(types.NUMBER, 34);
+        create_Experiment(types.NUMBER, 35);
+        create_Experiment(types.NUMBER, 36);
+        create_Experiment(types.NUMBER, 37);
+        create_Experiment(types.NUMBER, 38);
+        create_Experiment(types.NUMBER, 39);
+        create_Experiment(types.NUMBER, 40);
 
     }
 }
