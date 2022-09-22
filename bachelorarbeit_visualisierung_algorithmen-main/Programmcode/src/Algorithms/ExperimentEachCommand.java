cbd519559e3dba35f10239417c4293be3f903781
cdohmen <cdohmen>
2022-09-22 21:01:50 +0200

    reworked

--- bachelorarbeit_visualisierung_algorithmen-main/Programmcode/src/Algorithms/ExperimentEachCommand.java
+++ bachelorarbeit_visualisierung_algorithmen-main/Programmcode/src/Algorithms/ExperimentEachCommand.java
@@ -11,7 +11,7 @@ public class ExperimentEachCommand extends AbstractAlgorithm {
     public void executeAlgorithm() throws InterruptedException {
 
         Experiment experiment1 = create_Experiment(types.NUMBER, 12);
-        Experiment experiment2 = create_Experiment(types.NUMBER, 5);
+//        Experiment experiment2 = create_Experiment(types.NUMBER, 5);
 
         experiment1.insertElementAtEnd(11);
         experiment1.insertElementAtEnd(12);
@@ -25,11 +25,9 @@ public class ExperimentEachCommand extends AbstractAlgorithm {
         experiment1.swapElements(0, 2);
         experiment1.swapElements(2, 1);
 
-        experiment1.deleteExperiment();
-
+        // experiment1.deleteExperiment();
         // here an exception is thrown because it can't handle the actions on the second array after deleting the first
-
-        experiment2.insertElementAtEnd(21);
+        // experiment2.insertElementAtEnd(21);
 
     }
 
