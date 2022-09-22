7dc0c75e4d912aa8b2dfc2875dbdc15e704145a6
cdohmen <cdohmen>
2022-09-22 21:33:20 +0200

    added createInstantTranslate() which fixed the problem with too fast inputs

--- bachelorarbeit_visualisierung_algorithmen-main/Programmcode/src/visualization/animationCreation/ExperimentAnimation.java
+++ bachelorarbeit_visualisierung_algorithmen-main/Programmcode/src/visualization/animationCreation/ExperimentAnimation.java
@@ -4,6 +4,7 @@ import javafx.animation.FadeTransition;
 import javafx.animation.ParallelTransition;
 import javafx.animation.Transition;
 import javafx.animation.TranslateTransition;
+import javafx.scene.Node;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.util.Duration;
@@ -12,7 +13,12 @@ import java.util.ArrayList;
 
 /**
  * Seperated class for creating animations for ExperimentVisualization.
- * <p></p>
+ * <p>
+ *     Note: Directly changing the translation of a node with node.setTranslateX() or
+ *     node.setTranslateY() can cause errors if the input is too fast. Instead, create a
+ *     TranslateTransition with the duration of zero. You can use the function
+ *     {@link #createInstantTranslate(Node, int, int)}.
+ * <p>
  * <a href="https://docs.oracle.com/javase/8/javafx/api/javafx/animation/ParallelTransition.html">Documentation</a>
  */
 public class ExperimentAnimation {
@@ -23,10 +29,12 @@ public class ExperimentAnimation {
      */
     public Transition forCreateExperiment(VBox visualizedArray) {
 
+        // create instant translate transition for the correct start point
+        TranslateTransition instantTranslate = createInstantTranslate(visualizedArray, 100, 0);
+
         // create translate transition
         TranslateTransition translate = new TranslateTransition();
         translate.setDuration(Duration.millis(800));
-        visualizedArray.setTranslateX(100);
         translate.setByX(-100);
 
         // create fade transition
@@ -36,7 +44,7 @@ public class ExperimentAnimation {
         fade.setToValue(1);
 
         // create parallel transition with the two preceding transitions
-        return new ParallelTransition(visualizedArray, translate, fade);
+        return new ParallelTransition(visualizedArray, instantTranslate, translate, fade);
     }
 
     /**
@@ -47,10 +55,12 @@ public class ExperimentAnimation {
 
         // TODO: 22.09.2022 doesn't work, maybe because the array is completely removed before the animations are played
 
+        // create instant translate transition for the correct start point
+        TranslateTransition instantTranslate = createInstantTranslate(visualizedArray, -100, 0);
+
         // create translate transition
         TranslateTransition translate = new TranslateTransition();
         translate.setDuration(Duration.millis(800));
-        visualizedArray.setTranslateX(-100);
         translate.setByX(100);
 
         // create fade transition
@@ -59,8 +69,8 @@ public class ExperimentAnimation {
         fade.setFromValue(1);
         fade.setToValue(0);
 
-        // create parallel transition with the two preceding transitions
-        return new ParallelTransition(visualizedArray, translate, fade);
+        // create parallel transition and add all transitions
+        return new ParallelTransition(visualizedArray, instantTranslate, translate, fade);
     }
 
     /**
@@ -71,11 +81,13 @@ public class ExperimentAnimation {
      */
     public Transition forInsertElement(Text insertedValue, ArrayList<Text> movedValues) {
 
+        // create instant translate transition for the correct start point
+        TranslateTransition instantTranslateInserted = createInstantTranslate(insertedValue, 0, -25);
+
         // create translate transition for the inserted value
         TranslateTransition translateInserted = new TranslateTransition();
         translateInserted.setNode(insertedValue);
         translateInserted.setDuration(Duration.millis(300));
-        insertedValue.setTranslateY(-25);
         translateInserted.setByY(25);
 
         // create fade transition for the inserted value
@@ -85,21 +97,44 @@ public class ExperimentAnimation {
         fadeInserted.setFromValue(0);
         fadeInserted.setToValue(1);
 
-        ParallelTransition parallel = new ParallelTransition(translateInserted, fadeInserted);
+        ParallelTransition parallel = new ParallelTransition(instantTranslateInserted, translateInserted, fadeInserted);
 
         // for each moved Value, create a translation to the next field to the right
         for (Text moveValue : movedValues) {
+            // create instant translate transition for the correct start point
+            TranslateTransition instantTranslateMoved = createInstantTranslate(moveValue, -50, 0);
+
             // create translate transition
             TranslateTransition translateMoved = new TranslateTransition();
             translateMoved.setNode(moveValue);
             translateMoved.setDuration(Duration.millis(300));
-            moveValue.setTranslateX(-50);
             translateMoved.setByX(50);
-            // add to the parallelTransition
+
+            // add them to the parallelTransition
+            parallel.getChildren().add(instantTranslateMoved);
             parallel.getChildren().add(translateMoved);
         }
 
         return parallel;
+    }
+
+
+    /**
+     * Directly changing the translation of a node with node.setTranslateX() can cause
+     * errors if the input is too fast. Instead, create a TranslateTransition with a
+     * duration of zero.
+     * @param node the node to which the TranslateTransition is to be applied
+     * @param x the amount of the translation in x direction
+     * @param y the amount of the translation in y direction
+     * @return a TranslateTransition with a duration of zero
+     */
+    private TranslateTransition createInstantTranslate(Node node, int x, int y) {
+        TranslateTransition instantTranslate = new TranslateTransition();
+        instantTranslate.setNode(node);
+        instantTranslate.setByX(x);
+        instantTranslate.setByY(y);
+        instantTranslate.setDuration(Duration.ZERO);
+        return instantTranslate;
     }
 
 }
