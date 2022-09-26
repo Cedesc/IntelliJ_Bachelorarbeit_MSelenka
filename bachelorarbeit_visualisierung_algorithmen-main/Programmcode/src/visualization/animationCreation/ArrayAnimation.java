76d9118b63da9e6ac78d9ce894850246f5b7c733
Christian <irgendwas@nix.de>
2022-09-26 16:47:12 +0200

    created Animation classes

---
+++ bachelorarbeit_visualisierung_algorithmen-main/Programmcode/src/visualization/animationCreation/ArrayAnimation.java
@@ -0,0 +1,9 @@
+package visualization.animationCreation;
+
+/**
+ * Seperated class for creating animations for {@link visualization.ArrayVisualization}.
+ * <p>
+ * <a href="https://docs.oracle.com/javase/8/javafx/api/javafx/animation/ParallelTransition.html">Documentation</a>
+ */
+public class ArrayAnimation extends AbstractAnimationCreator {
+}
---
+++ bachelorarbeit_visualisierung_algorithmen-main/Programmcode/src/visualization/animationCreation/VariableAnimation.java
@@ -0,0 +1,9 @@
+package visualization.animationCreation;
+
+/**
+ * Seperated class for creating animations for {@link visualization.VariableVisualization}.
+ * <p>
+ * <a href="https://docs.oracle.com/javase/8/javafx/api/javafx/animation/ParallelTransition.html">Documentation</a>
+ */
+public class VariableAnimation extends AbstractAnimationCreator {
+}
