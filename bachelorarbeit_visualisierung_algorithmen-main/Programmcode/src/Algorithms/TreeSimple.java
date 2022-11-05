00da583d7267aac6f00deafab96509354cda41ed
cdohmen <cdohmen>
2022-11-05 11:34:46 +0100

    HelloWorld test for tree visualization (working)

---
+++ bachelorarbeit_visualisierung_algorithmen-main/Programmcode/src/Algorithms/TreeSimple.java
@@ -0,0 +1,17 @@
+package Algorithms;
+
+import abstractAlgorithmus.AbstractAlgorithm;
+import datastructures.Tree;
+import supportClasses.treeClasses.MyNode;
+import supportClasses.types;
+
+public class TreeSimple extends AbstractAlgorithm {
+
+    @Override
+    public void executeAlgorithm() throws InterruptedException {
+
+        Tree tree1 = create_Tree(types.NUMBER, new MyNode());
+
+    }
+
+}
