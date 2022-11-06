554dec62e332b44a999014c4eb893183a78ca5bb
cdohmen <cdohmen>
2022-11-06 22:49:39 +0100

    updated

--- bachelorarbeit_visualisierung_algorithmen-main/Programmcode/src/Algorithms/TreeSimple.java
+++ bachelorarbeit_visualisierung_algorithmen-main/Programmcode/src/Algorithms/TreeSimple.java
@@ -10,24 +10,30 @@ public class TreeSimple extends AbstractAlgorithm {
     @Override
     public void executeAlgorithm() throws InterruptedException {
 
-        Tree tree1 = create_Tree(types.NUMBER,
-                new MyNode(0, 33, null, null, null, 15, 15));
+//        Tree tree1 = create_Tree(types.NUMBER,
+//                new MyNode(0, 33, null, null, null, 15, 15));
 
         MyNode node4 =
-                new MyNode(4, 444, null, null, null, 300, 100);
+                new MyNode(4, 444, 300, 100);
         MyNode node3 =
-                new MyNode(3, 333, null, null, null, 200, 200);
+                new MyNode(3, 333, 200, 200);
         MyNode node2 =
-                new MyNode(2, 222, null, node4, node3, 200, 100);
+                new MyNode(2, 222, 200, 100);
         MyNode node1 =
-                new MyNode(1, 111, null, node2, null, 50, 100);
+                new MyNode(1, 111, 50, 100);
         MyNode node0 =
-                new MyNode(0, 24, null, null, node1, 100, 10);
+                new MyNode(0, 24, 100, 10);
+
 
         Tree tree2 = create_Tree(types.NUMBER, node0);
 
+        tree2.addLeaf(node0, node1);
+        tree2.addLeaf(node0, node2);
+        tree2.addLeaf(node0, node4);
+        tree2.addLeaf(node2, node3);
+
         MyNode node5 =
-                new MyNode(5, 555, null, null, null, 100, 400);
+                new MyNode(5, 555, 100, 400);
 
         tree2.addLeaf(node3, node5);
 
