159987ddb6265de9c12865f408f480bc9214dea3
cdohmen <cdohmen>
2022-11-06 13:20:37 +0100

    updated

--- bachelorarbeit_visualisierung_algorithmen-main/Programmcode/src/Algorithms/TreeSimple.java
+++ bachelorarbeit_visualisierung_algorithmen-main/Programmcode/src/Algorithms/TreeSimple.java
@@ -13,6 +13,19 @@ public class TreeSimple extends AbstractAlgorithm {
         Tree tree1 = create_Tree(types.NUMBER,
                 new MyNode(0, 33, null, null, null, 15, 15));
 
+        MyNode node4 =
+                new MyNode(4, 444, null, null, null, 300, 100);
+        MyNode node3 =
+                new MyNode(3, 333, null, null, null, 200, 200);
+        MyNode node2 =
+                new MyNode(2, 222, null, node4, node3, 200, 100);
+        MyNode node1 =
+                new MyNode(1, 111, null, node2, null, 50, 100);
+        MyNode node0 =
+                new MyNode(0, 24, null, null, node1, 100, 10);
+
+        Tree tree2 = create_Tree(types.NUMBER, node0);
+
     }
 
 }
