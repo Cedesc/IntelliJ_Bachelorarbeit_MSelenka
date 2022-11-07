bb4c427662358f880f9b0dc3598e83a4744e3819
cdohmen <cdohmen>
2022-11-07 14:20:27 +0100

    added

---
+++ bachelorarbeit_visualisierung_algorithmen-main/Programmcode/src/Algorithms/TreeRandom.java
@@ -0,0 +1,36 @@
+package Algorithms;
+
+import abstractAlgorithmus.AbstractAlgorithm;
+import datastructures.Tree;
+import supportClasses.treeClasses.MyNode;
+import supportClasses.types;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class TreeRandom extends AbstractAlgorithm {
+
+    @Override
+    public void executeAlgorithm() throws InterruptedException {
+
+        int numberOfNodes = 50;
+
+        ArrayList<MyNode> nodes = new ArrayList<>(numberOfNodes);
+
+        // create nodes
+        for (int i = 0 ; i <= numberOfNodes ; i++) {
+            nodes.add(new MyNode(i));
+        }
+
+        // create tree
+        Tree tree = create_Tree(types.NUMBER, nodes.get(0));
+
+
+        for (int i = 1 ; i < numberOfNodes ; i++) {
+            int randomInt = new Random().nextInt(i);
+            tree.addLeaf(nodes.get(randomInt), nodes.get(i));
+        }
+
+
+    }
+}
