546e649f2c63fcfadb5c2acb5a3bfe193ef9a3a6
Cedesc <christiandohmen1910@gmail.com>
2022-11-08 12:24:07 +0100

    updated and added xDistance, yDistance and nodeSize

--- bachelorarbeit_visualisierung_algorithmen-main/Programmcode/src/supportClasses/config/TempConfig.java
+++ bachelorarbeit_visualisierung_algorithmen-main/Programmcode/src/supportClasses/config/TempConfig.java
@@ -1,5 +1,8 @@
 package supportClasses.config;
 
+import controller.SelectAlgorithmController;
+import javafx.animation.Transition;
+import javafx.stage.Stage;
 import javafx.util.Duration;
 
 public class TempConfig {
@@ -7,7 +10,7 @@ public class TempConfig {
     /**
      * Filepath of the Algorithms package.
      * <p>
-     * Used in {@link controller.SelectAlgorithmController}
+     * Used in {@link SelectAlgorithmController#createList()}
      */
     public static final String PATH_TO_ALGORITHMS =
             "bachelorarbeit_visualisierung_algorithmen-main/Programmcode/src/Algorithms";
@@ -17,7 +20,7 @@ public class TempConfig {
     /**
      * The width of the window.
      * <p>
-     * Used in {@link model.ParentViewModel}
+     * Used in {@link model.ParentViewModel#start(Stage)}
      */
     public static final double WINDOW_WIDTH =
             1200;
@@ -25,7 +28,7 @@ public class TempConfig {
     /**
      * The height of the window.
      * <p>
-     * Used in {@link model.ParentViewModel}
+     * Used in {@link model.ParentViewModel#start(Stage)}
      */
     public static final double WINDOW_HEIGHT =
             800;
@@ -33,7 +36,7 @@ public class TempConfig {
     /**
      * Whether the window should be maximized or not.
      * <p>
-     * Used in {@link model.ParentViewModel}
+     * Used in {@link model.ParentViewModel#start(Stage)}
      */
     public static final boolean MAXIMIZED_WINDOW =
             false;
@@ -42,7 +45,7 @@ public class TempConfig {
     /**
      * The distance between the elements.
      * <p>
-     * Used in {@link visualization.animationCreation.AbstractAnimationCreator}
+     * Used in {@link visualization.animationCreation.AbstractAnimationCreator#fieldDistance}
      */
     public static final int FIELD_DISTANCE =
             50;
@@ -50,7 +53,7 @@ public class TempConfig {
     /**
      * The standard duration of animations. Not every animation has this duration, but each is oriented to it.
      * <p>
-     * Used in {@link visualization.animationCreation.AbstractAnimationCreator}
+     * Used in {@link visualization.animationCreation.AbstractAnimationCreator#standardDuration}
      */
     public static final Duration STANDARD_DURATION =
             Duration.millis(100);
@@ -59,7 +62,7 @@ public class TempConfig {
     /**
      * The maximal possible zoom.
      * <p>
-     * Used in {@link supportClasses.zooming.SceneGestures}
+     * Used in {@link supportClasses.zooming.SceneGestures#MAX_SCALE}
      */
     public static final double MAX_ZOOM_SCALE =
             5;
@@ -67,9 +70,28 @@ public class TempConfig {
     /**
      * The minimal possible zoom.
      * <p>
-     * Used in {@link supportClasses.zooming.SceneGestures}
+     * Used in {@link supportClasses.zooming.SceneGestures#MIN_SCALE}
      */
     public static final double MIN_ZOOM_SCALE =
             0.1;
+
+
+    /**
+     * For visualization of trees. The x-distance between nodes.
+     * Used in {@link visualization.TreeVisualization#xDistance}
+     */
+    public static final int NODE_X_DISTANCE = 75;
+
+    /**
+     * For visualization of trees. The y-distance between nodes.
+     * Used in {@link visualization.TreeVisualization#yDistance}
+     */
+    public static final int NODE_Y_DISTANCE = 75;
+
+    /**
+     * For visualization of trees. The node size.
+     * Used in {@link visualization.TreeVisualization#nodeSize}
+     */
+    public static final int NODE_SIZE = 50;
 
 }
