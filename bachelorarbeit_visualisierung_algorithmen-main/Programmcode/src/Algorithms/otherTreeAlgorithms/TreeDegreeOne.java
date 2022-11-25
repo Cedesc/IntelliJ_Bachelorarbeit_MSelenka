780070ddc9efc8fe3c68191578d64c2a4082d37f
cdohmen <cdohmen>
2022-11-25 13:46:07 +0100

    minor

--- bachelorarbeit_visualisierung_algorithmen-main/Programmcode/src/Algorithms/otherTreeAlgorithms/TreeDegreeOne.java
+++ bachelorarbeit_visualisierung_algorithmen-main/Programmcode/src/Algorithms/otherTreeAlgorithms/TreeDegreeOne.java
@@ -10,12 +10,12 @@ public class TreeDegreeOne extends AbstractAlgorithm {
     @Override
     public void executeAlgorithm() throws InterruptedException {
 
-        MyNode node0 = new MyNode(0, 0, 0, 50);
-        MyNode node1 = new MyNode(1, 11, 100, 50);
-        MyNode node2 = new MyNode(2, 22, 200, 50);
-        MyNode node3 = new MyNode(3, 33, 300, 50);
-        MyNode node4 = new MyNode(4, 44, 400, 50);
-        MyNode node5 = new MyNode(5, 55, 500, 50);
+        MyNode node0 = new MyNode(0, 0);
+        MyNode node1 = new MyNode(1, 11);
+        MyNode node2 = new MyNode(2, 22);
+        MyNode node3 = new MyNode(3, 33);
+        MyNode node4 = new MyNode(4, 44);
+        MyNode node5 = new MyNode(5, 55);
 
         Tree tree = create_Tree(types.NUMBER, node0);
 
